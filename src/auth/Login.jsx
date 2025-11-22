8836229bc36b712a0d1dd8f4de98a5b3375779c5
ukachar <83542678+ukachar@users.noreply.github.com>
2025-11-22 12:36:19 +0100

    ID na login polja

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -51,17 +51,20 @@ const Login = () => {
                     <input
                       className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
                       type="email"
+                      id="login_mail"
                       placeholder="Email"
                       onChange={(e) => setEmail(e.target.value)}
                     />
                     <input
                       className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
                       type="password"
+                      id="login_password"
                       placeholder="Lozinka"
                       onChange={(e) => setPassword(e.target.value)}
                     />
                     <button
                       type="submit"
+                      id="login_submit"
                       className="mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none"
                     >
                       Prijava
